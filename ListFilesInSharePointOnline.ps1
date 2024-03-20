dc7f7a659996c915c90da0ac5e52d075a1e2d37a
TransonicFrame <austiny4@ptd.net>
2024-03-20 00:31:02 -0400

    Working changes to scripts.

--- ListFilesInSharePointOnline.ps1
+++ ListFilesInSharePointOnline.ps1
@@ -3,29 +3,19 @@ param (
     [string]$TenantUrl,
     [string]$ClientId,
     [string]$CertificationPath,
-    [SecureString]$CertificationPassword,
-    [string]$ListName
+    [string]$CertificationPassword,
+    [string]$Folder
 )
 
 #Connects to server
-Connect-PnPOnline -ClientId $ClientId -CertificatePath $CertificationPath -CertificatePassword $CertificationPassword -Url $SiteUrl -Tenant $TenantUrl 
+Connect-PnPOnline -ClientId $ClientId -CertificatePath $CertificationPath -CertificatePassword (ConvertTo-SecureString -AsPlainText $CertificationPassword -Force) -Url $SiteUrl -Tenant $TenantUrl 
 
-#Define Query to Filter
-$Query= "<View>
-            <Query>
-                <Where>
-                    <Gt>
-                        <FieldRef Name='Modified' Type='DateTime'/>
-                        <Value Type='DateTime' IncludeTimeValue='TRUE'>
-                            <Today OffsetDays='-1'/>
-                        </Value>
-                    </Gt>
-                </Where>
-            </Query>
-        </View>"
- 
-$ListItems = Get-PnPListItem -List $ListName -Query $Query
-Write-host "Total Number of Items Found:"$ListItems.count
- 
-#Get Each Item's Modified Date
-$ListItems | ForEach-Object { Write-host ("List Item:{0} was Modified on {1}" -f $_["FileLeafRef"],$_["Modified"]) }
+# Get files in the folder
+$files = Get-PnPFolderItem -FolderSiteRelativeUrl $Folder -ItemType File
+
+# Output file names
+foreach ($file in $files) {
+    Write-Host $file.Name
+}
+
+Disconnect-PnPOnline
--- SyncFilesWithNAS.ps1
+++ SyncFilesWithNAS.ps1
@@ -2,11 +2,56 @@ param (
     [string]$SiteUrl,
     [string]$TenantUrl,
     [string]$ClientId,
-    [string]$CertificationPath,
-    [SecureString]$CertificationPassword,
-    [string]$ListName
+    [string]$CertificatePath,
+    [string]$CertificatePassword
 )
 
-#Connects to server
-Connect-PnPOnline -ClientId $ClientId -CertificatePath $CertificationPath -CertificatePassword $CertificationPassword -Url $SiteUrl -Tenant $TenantUrl 
+Function Backup-Entire-SPOFolder([Microsoft.SharePoint.Client.Folder]$Folder, $DestinationFolder)
+{ 
+    $FolderURL = $Folder.ServerRelativeUrl.Substring($Folder.Context.Web.ServerRelativeUrl.Length)
+    $LocalFolder = $DestinationFolder + ($FolderURL -replace "/","\")
+    If (!(Test-Path -Path $LocalFolder)) {
+            New-Item -ItemType Directory -Path $LocalFolder | Out-Null
+            Write-host -f Yellow "Created a New Folder '$LocalFolder'"
+    }
+           
+    $FilesColl = Get-PnPFolderItem -FolderSiteRelativeUrl $FolderURL -ItemType File
+    Foreach($File in $FilesColl)
+    {	
+        $LocalFilePath = Join-Path -Path $LocalFolder -ChildPath $File.Name
+        if (-not (Test-Path -Path $LocalFilePath)) {
+            Get-PnPFile -ServerRelativeUrl $File.ServerRelativeUrl -Path $LocalFolder -FileName $File.Name -AsFile -Force
+            Write-host -f Green "`tDownloaded File from '$($File.ServerRelativeUrl)'"
+        }
+        else {
+            $SharePointFile = Get-PnPFile -Url $File.ServerRelativeUrl
+            $LocalFile = Get-Item $LocalFilePath
+            if ($SharePointFile.TimeLastModified -gt $LocalFile.LastWriteTime) {
+                Get-PnPFile -ServerRelativeUrl $File.ServerRelativeUrl -Path $LocalFolder -FileName $File.Name -AsFile -Force
+                Write-host -f Green "`tUpdated File from '$($File.ServerRelativeUrl)'"
+            }
+            else {
+                Write-host -f Cyan "`tSkipped File '$($File.ServerRelativeUrl)' as it is up to date."
+            }
+        }
+    }
+    
+    $SubFolders = Get-PnPFolderItem -FolderSiteRelativeUrl $FolderURL -ItemType Folder
+    Foreach ($SubFolder in $SubFolders | Where-Object {$_.Name -ne "Forms"})
+    {
+        Backup-Entire-SPOFolder $SubFolder $DestinationFolder
+    }
+} 
 
+#Download Variables
+$LibraryURL = "Shared Documents"
+$DownloadPath = "F:\Users\austi\Downloads\Test"
+
+#Connects to server
+Connect-PnPOnline -ClientId $ClientId -CertificatePath $CertificatePath -CertificatePassword (ConvertTo-SecureString -AsPlainText $CertificatePassword -Force) -Url $SiteUrl -Tenant $TenantUrl 
+
+#Gets Library Folder
+$Folder = Get-PnPFolder -Url $LibraryURL
+
+#Calls Function
+Backup-Entire-SPOFolder $Folder $DownloadPath
